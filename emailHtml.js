1461875228413d25c4d0b7a2cfb80cbad76072ad
zailu <919422364@qq.com>
2021-12-01 15:10:55 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -10,7 +10,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
-      <div style="font-size: 0.5rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(0, 0, 0, 0.5);">
+      <div style="font-size: 1rem;width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(0, 0, 0, 0.5);">
         <!-- 天数 -->
         <div style="font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
           <!-- <p>今天是在一起的第${lovingDays}天！</p>-->
@@ -27,7 +27,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
           ${content}
         </div>
         <!-- 天气 -->
-        <div style="padding: 1.2rem 1rem;">
+        <div style="font-size: 1rem;padding: 1.2rem 1rem;">
           <div style="font-weight: bold;color: #000000;">厦门今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</div>
           <ul class="list">
             <li class="dress">
