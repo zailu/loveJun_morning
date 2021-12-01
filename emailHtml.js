7ca3cb93425bf29b7286e713022cbf47bdbc0fd5
zailu <919422364@qq.com>
2021-12-01 15:05:10 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -12,22 +12,23 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
       <body>
       <div style="width: 100%;height: 100%;overflow-y: scroll;box-sizing: border-box;border:1px solid rgba(0, 0, 0, 0.5);">
         <!-- 天数 -->
-        <div style="font-size: 2rem;font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 1.5rem;letter-spacing: 0.2rem;">
+        <div style="font-size: 2px;font-weight: bold;line-height: 2;padding: 1rem;margin: 1rem;border-radius: 1rem 0.5rem;text-align: center;position: relative;color: #000000;margin-bottom: 2px;letter-spacing: 0.2rem;">
           <!-- <p>今天是在一起的第${lovingDays}天！</p>-->
           <p>今天是想你的第${lovingDays}天！</p>
+		  <div>今天也要开开心心，元气满满哦</div>
+		  <div>(*￣︶￣)！</div>
         </div>
         <!-- 图片 -->
-        <div style="width: 100%;max-height: 35rem;overflow: hidden;padding: 0 1rem;box-sizing: border-box;border-radius: 0.6rem;position: relative;display: flex;justify-content: center;align-items: center;padding-bottom: 3.4rem;">
-          <img style="width: 100%; max-width: 768px"; src="${imgurl}" alt="图片" />
-          <div>今天也要开开心心，元气满满哦(*￣︶￣)！</div>
+        <div style="width: 100%;max-height: 35rem;overflow: hidden;padding: 0 1rem;box-sizing: border-box;border-radius: 0.6rem;position: relative;display: flex;justify-content: center;align-items: center;padding-bottom: 1rem;">
+          <img style="width: 100%;"; src="${imgurl}" alt="图片" />
         </div>
         <!-- 每日一句 -->
-        <div style="font-size: 1.6rem;text-indent: 2em;font-style: italic;color: #000000;text-align:center;padding: 1.5rem 1rem;">
+        <div style="font-size: 1.6rem;text-indent: 2em;font-style: italic;color: #000000;text-align:center;padding: 1.5px 1px;">
           ${content}
         </div>
         <!-- 天气 -->
-        <div style="font-size: 1.4rem;padding: 1.2rem 1rem;">
-          <div style="font-weight: bold;margin-bottom: 1.5rem;position: relative;padding-left: 2.5rem;line-height: 2.4rem;color: #000000;">厦门今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</div>
+        <div style="padding: 1.2rem 1rem;">
+          <div style="font-weight: bold;color: #000000;">厦门今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C</div>
           <ul class="list">
             <li class="dress">
               ${daily[1].name}(${daily[1].category}):
