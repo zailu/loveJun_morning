00d47c7a392992d6df6251bccda0a413939e5937
zailu <919422364@qq.com>
2021-12-01 10:54:38 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -8,7 +8,7 @@ function fn(weatherData, lifeData, content, imgurl, lovingDays) {
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="emailHtml.css">
+        <link rel="stylesheet" type = "text/css" href="emailHtml.css">
       </head>
       <body>
       <div class="container">
