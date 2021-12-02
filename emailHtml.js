271a0d4b751242ed0ce26bbc077cdafaa8734d5c
zailu <919422364@qq.com>
2021-12-02 17:32:27 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -17,7 +17,7 @@ function fn(weatherData, lifeData, content, source, author, imgurl, lovingDays) 
           <p>今天是想你的第${lovingDays}天！</p>
 		  <div>今天也要开开心心，元气满满哦</div>
 		  <div>(*￣︶￣)！</div>
-		  <img src="love.png" />
+		  <img src="../love.png" />
         </div>
         <!-- 图片 -->
         <div style="width: 100%;max-height: 35rem;overflow: hidden;padding: 0 1rem;box-sizing: border-box;border-radius: 0.6rem;position: relative;display: flex;justify-content: center;align-items: center;padding-bottom: 1rem;">
