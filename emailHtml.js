224bbd57c60bfb7797ceed29b8ffbe6e1d2f9a21
zailu <919422364@qq.com>
2021-12-02 15:46:25 +0800

    LoveXuan

--- emailHtml.js
+++ emailHtml.js
@@ -1,4 +1,4 @@
-function fn(weatherData, lifeData, content,source, imgurl, lovingDays) {
+function fn(weatherData, lifeData, content, source, imgurl, lovingDays) {
   const { daily: weatherDataDaily } = weatherData;
   const { daily } = lifeData;
 
