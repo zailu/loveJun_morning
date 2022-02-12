435cdc457cc3c2a55d09603355d3443212eec262
zailu <919422364@qq.com>
2022-02-12 18:27:46 +0800

    test

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ module.exports = {
   to: '919422364@qq.com', // 发送到谁，填邮箱
   //to: '1594072713@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: '101280604',  // 和风天气-厦门的id 101230201   深圳的id 101280604
+  location: 'CN101280301',  // 和风天气-厦门的id 101230201   深圳的id 101280604  惠州id CN101280301
+  //查询所在地id ： https://free-api.heweather.net/s6/weather/now?key=33369e365fe84eb68876f52a2ae51cca&location=深圳
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
   startDay: '2016-10-12', // 在一起的日期
--- emailHtml.js
+++ emailHtml.js
@@ -38,7 +38,7 @@ function fn(weatherData, lifeData, content, source, author, imgurl, lovingDays) 
         <div style="font-size: 1rem;padding: 1.2rem 1rem;">
 			<div style="font-weight: bold;color: #000000;padding: 1rem 1rem;">
 				<img style="width: 10%;"; src="https://s3.bmp.ovh/imgs/2021/12/5a70dee63635fcdc.png" />
-				深圳今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C
+				惠州今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C
 			</div>
 			<div style="font-size: 1rem;float:right;padding: 1rem 1rem;">
 				<div style="padding-bottom: 1rem;">
