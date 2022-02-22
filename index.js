d12d3eadbcb50b9c46c4c4594c37bb4267e535ca
zailu <919422364@qq.com>
2022-02-22 19:19:46 +0800

    test

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ async function init() {
 	//获取文字
 	const oneRes2 = await fetch(
       //早安心语
-	  //`http://api.tianapi.com/zaoan/index?key=10a0b8b423af1448982e50a6909e0230`
+	  `http://api.tianapi.com/zaoan/index?key=10a0b8b423af1448982e50a6909e0230`
 	  //古代情诗
-	  `http://api.tianapi.com/qingshi/index?key=10a0b8b423af1448982e50a6909e0230`
+	  //`http://api.tianapi.com/qingshi/index?key=10a0b8b423af1448982e50a6909e0230`
 	);
     const oneData2 = await oneRes2.json();
-    const { content, source, author } = oneData2.newslist[0];
+    const { content } = oneData2.newslist[0];
 
     // 计算日期
     const lovingDays = dayjs(dayjs().tz('Asia/Shanghai')).diff(
