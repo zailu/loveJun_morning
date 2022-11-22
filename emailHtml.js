babf914363ea2361b56dd01097c31f7a8c491f35
jack_xue <919422364@qq.com>
2022-11-22 23:17:07 +0800

    test

--- emailHtml.js
+++ emailHtml.js
@@ -17,14 +17,14 @@ function fn(weatherData, lifeData, content, source, author, imgurl, lovingDays) 
           <p>今天已经是相识的第${lovingDays}天啦</p>
 		  <div>今天也要开开心心，元气满满哦</div>
 		  <!-- <img  style="width: 50%;"; src="https://s3.bmp.ovh/imgs/2021/12/c78f040792460398.png" /> 爱心-->
-		  <img  style="width: 50%;"; src="https://s3.bmp.ovh/imgs/2022/02/740c380849b7e962.png" />
+		  <img  style="width: 50%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/9eba2caaa41e9f31.png" />
         </div>
         <!-- 图片 -->
         <div style="width: 100%;max-height: 35rem;overflow: hidden;padding: 0 1rem;box-sizing: border-box;border-radius: 0.6rem;position: relative;display: flex;justify-content: center;align-items: center;padding-bottom: 1rem;">
           <img style="width: 100%;"; src="${imgurl}" alt="图片" />
         </div>
         <!-- 每日一句 -->
-        <div style="font-size: 1rem;text-indent: 2em;font-style: italic;padding: 3rem 2rem;background: url(https://s3.bmp.ovh/imgs/2021/12/b8b68c4a645b0f91.png) no-repeat center top;background-size: 90% 100%;">
+        <div style="font-size: 1rem;text-indent: 2em;font-style: italic;padding: 3rem 2rem;background: url(https://s3.uuu.ovh/imgs/2022/11/22/386d5a5da2dc44d7.png) no-repeat center top;background-size: 90% 100%;">
 			<!-- https://s3.bmp.ovh/imgs/2021/12/b8b68c4a645b0f91.png    云朵-->
 			<div style="padding: 2rem 2rem;font-family: "仿宋";color: #FFFFFF;">
 				 ${content}
@@ -37,22 +37,22 @@ function fn(weatherData, lifeData, content, source, author, imgurl, lovingDays) 
         <!-- 天气 -->
         <div style="font-size: 1rem;padding: 1.2rem 1rem;">
 			<div style="font-weight: bold;color: #000000;padding: 1rem 1rem;">
-				<img style="width: 10%;"; src="https://s3.bmp.ovh/imgs/2021/12/5a70dee63635fcdc.png" />
+				<img style="width: 10%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/7439d882f2a7c9e5.png" />
 				惠州今日气温:${weatherDataDaily[0].tempMin}°C - ${weatherDataDaily[0].tempMax}°C
 			</div>
 			<div style="font-size: 1rem;float:right;padding: 1rem 1rem;">
 				<div style="padding-bottom: 1rem;">
-					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/f4e0eace95359e80.png" />
+					<img style="width: 6%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/1e53ba51adba2c50.png" />
 					${daily[1].name}(${daily[1].category}):
 					${daily[1].text}
 				</div>
 				<div style="padding-bottom: 1rem;">
-					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/678c04dd55d73f81.png" />
+					<img style="width: 6%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/d790de86077dec0c.png" />
 					${daily[2].name}(${daily[2].category}):
 					${daily[2].text}
 				</div>
 				<div style="padding-bottom: 1rem;">
-					<img style="width: 6%;"; src="https://s3.bmp.ovh/imgs/2021/12/f63a46c72a12aca3.png" />
+					<img style="width: 6%;"; src="https://s3.uuu.ovh/imgs/2022/11/22/43443b3058ced63a.png" />
 					${daily[0].name}(${daily[0].category}):
 					${daily[0].text}
 				</div>
