b6c28562294b9a151988fedc100508551c59b99a
jack_xue <919422364@qq.com>
2022-11-23 19:25:18 +0800

    test

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   fromDisplaySubText: '早安', // 收件箱展示的次级标题
   user: '919422364@qq.com', // 发送者邮箱
   pass: 'xkhoaxouqlfjbbif', // 发送者邮箱MTP协议密码
-  to: '919422364@qq.com', // 发送到谁，填邮箱
-  //to: '1943979603@qq.com', // 发送到谁，填邮箱
+  //to: '919422364@qq.com', // 发送到谁，填邮箱
+  to: '1943979603@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: 'CN101280301',  // 和风天气-厦门的id 101230201   深圳的id 101280604  惠州id CN101280301
   //查询所在地id ： https://free-api.heweather.net/s6/weather/now?key=33369e365fe84eb68876f52a2ae51cca&location=深圳
